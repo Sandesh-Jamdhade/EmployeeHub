48855d93d4fb06f3400de3fecfc482edb94f86c5
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-01-25 17:50:48 +0530

    Updated Urls

--- EmployeeHub/urls.py
+++ EmployeeHub/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('employees.urls')),
-    path('login/',auth_views.LoginView.as_view(template_name='login.html')),
-    path('logout/',auth_views.LogoutView.as_view(template='logout.html')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html')),
+    path('logout/', auth_views.LogoutView.as_view()),
+    path('', include('employees.urls')),
+
 ]
