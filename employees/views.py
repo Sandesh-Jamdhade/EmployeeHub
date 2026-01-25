5176bace075404cc07541c9d2e998c5dbb2bb42f
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-01-25 16:48:10 +0530

    Adding CRUD Logic

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Employee
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def Dashboard(request):
+    employees= Employee.objects.all()
+    return render(request,'Dashboard.html',{'employees':employees})
+
+@login_required
+def add_employee(request):
+    if request.method == "POST":
+        Employee.objects.create(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            department=request.POST['department'],
+            salary=request.POST['salary']
+        )
+        return redirect('/')
+    return render(request,'add_employee.html')
+
+@login_required
+def edit_employee(request,id):
+    emp=Employee.objects.get(id=id)
+    if request.method=="POST":
+        emp.name=request.POST['name']
+        emp.email=request.POST['email']
+        emp.department=request.POST['department']
+        emp.salary=request.POST['salary']
+        emp.save()
+        return redirect('/')
+    return render(request,'edit_employee.html',{'emp':emp})
+
+@login_required
+def delete_employee(request,id):
+    Employee.objects.get(id=id).delete()
+    return redirect('/')
