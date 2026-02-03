6f63713ae1129ad4d691913eb519ce1112d173b6
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-03 13:25:21 +0530

    Path for the Forms of the Website

--- employees/urls.py
+++ employees/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('', views.dashboard),
-    path('add/', views.add_employee),
-    path('edit/<int:id>/', views.edit_employee),
-    path('delete/<int:id>/', views.delete_employee),
-    path('register/', views.register),
-    path('accounts/profile/', views.profile),
+urlpatterns = [
+    path("", views.login_view, name="login"),
+    path("register/", views.register_view, name="register"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("add/", views.add_employee, name="add_employee"),
+    path("edit/<int:id>/", views.edit_employee, name="edit_employee"),
+    path("delete/<int:id>/", views.delete_employee, name="delete_employee"),
+    path("logout/", views.logout_view, name="logout"),
+    path("profile/", views.profile, name="profile"),
+
 ]
+
