3aa8c2b82cb9ea495997aac72313d4c3ebe19615
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-03 13:24:28 +0530

    Keep Models empty because its frontend repo

--- employees/models.py
+++ employees/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Employee(models.Model):
-    name=models.CharField(max_length=50)
-    email=models.EmailField()
-    department=models.CharField(max_length=50)
-    salary=models.IntegerField()
 
-    def __str__(self):
-        return self.name
     
